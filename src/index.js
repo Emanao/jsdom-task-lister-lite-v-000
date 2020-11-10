d471f02704cbe03f2d74c00dc443c66f7c1168ef
Eva Navarro <emanao@me.com>
2020-11-10 20:34:56 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -41,3 +41,24 @@ function createDeleteButton(){
   });
   return button;
 }
+
+function createDropDown(){
+  let dropDownList = document.createElement("select");
+  dropDownList.setAttribute("id", "priority");
+  let option1 = document.createElement("option");
+  option1.setAttribute("value", "1");
+  option1.innerText="High";
+  dropDownList.appendChild(option1);
+  let option2 = document.createElement("option");
+  option2.setAttribute("value", "2");
+  option2.innerText="Medium";
+  dropDownList.appendChild(option2);
+  let option3 = document.createElement("option");
+  option3.setAttribute("value", "3");
+  option3.innerText="Low"
+  dropDownList.appendChild(option3);
+
+
+
+
+}
